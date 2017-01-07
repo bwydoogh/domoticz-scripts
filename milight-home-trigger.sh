1bf17c83815f80d925628b1b51953ce2f9773e26
Benny Wydooghe <benny.wydooghe@gmail.com>
2017-01-07 14:23:20 +0100

    Minor change bash script

--- milight-home-trigger.sh
+++ milight-home-trigger.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 if [[ "$1" == "ON" ]] || [[ "$1" == "OFF" ]]
 then
-    /home/pi/domoticz/scripts/milight-home.py "$1" 00 
-    sleep .5
-    /home/pi/domoticz/scripts/milight-home.py "$1" 07
+    /home/pi/domoticz/scripts/milight-home.py "$1" 00 && sleep .5 && /home/pi/domoticz/scripts/milight-home.py "$1" 07
 else
     /home/pi/domoticz/scripts/milight-home.py "$1" "$2"
 fi
