8a1fb10b6e177f198f8fe42904ee46293d735309
Benny Wydooghe <benny.wydooghe@gmail.com>
2017-01-08 20:43:41 +0100

    Minor improvement bash script

--- milight-home-trigger.sh
+++ milight-home-trigger.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [[ "$1" == "ON" ]] || [[ "$1" == "OFF" ]]
 then
+    # Special case: turn all (including the iBox) leds ON or OFF
+    # What happens are 2 script calls, once for each device type (currently 00 and 07)
     /home/pi/domoticz/scripts/milight-home.py "$1" 00 && sleep .5 && /home/pi/domoticz/scripts/milight-home.py "$1" 07
 else
-    /home/pi/domoticz/scripts/milight-home.py "$1" "$2"
+    # Regular case: pass all arguments to the Python script
+    /home/pi/domoticz/scripts/milight-home.py "$@"
 fi
